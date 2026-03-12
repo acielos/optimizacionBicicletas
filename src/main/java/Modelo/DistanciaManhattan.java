136c7df816a8f040e0aaf9ba75e5b9e80f641db8
acielos <antoniocielos@gmail.com>
2026-03-12 10:36:28 +0100

    v1.0.2

--- src/main/java/Modelo/DistanciaManhattan.java
+++ src/main/java/Modelo/DistanciaManhattan.java
@@ -1,28 +1,23 @@
 package main.java.Modelo;
 
+import main.java.DataTypes.Estacion;
+
 public class DistanciaManhattan {
-    private double distancia;
+    private double distancia_total;
     private double radio = 6371.0;
-    private double lat1, lat2, lon1, lon2;
 
-    // Constructor de la clase
-    public DistanciaManhattan(double lat1, double lon1, double lat2, double lon2) {
-        this.lat1 = lat1;
-        this.lon1 = lon1;
-        this.lat2 = lat2;
-        this.lon2 = lon2;
+    public double calculaDistancia(Estacion estacion1, Estacion estacion2) {
+        // Calculamos en radianes los variables que necesitamos
+        double dlat_rad = Math.toRadians(estacion2.getLatitud() - estacion1.getLatitud());
+        double dlon_rad = Math.toRadians(estacion2.getLongitud() - estacion1.getLongitud());
+        double lat_media_rad = Math.toRadians((estacion1.getLatitud() + estacion2.getLatitud()) / 2);
+        double dist_norte_sur = radio * dlat_rad;
+        double dist_este_oeste = radio * dlon_rad * Math.cos(lat_media_rad);
+
+        // Calculamos la distancia total entre dos puntos
+        distancia_total = dist_norte_sur + dist_este_oeste;
+
+        // Evidentemente devolvemos la distancia total entre los dos puntos
+        return distancia_total;
     }
-
-    // Calculamos en radianes los variables que necesitamos
-    double dlat_rad = Math.toRadians(lat2 - lat1);
-    double dlon_rad = Math.toRadians(lon2 - lon1);
-    double lat_media_rad = Math.toRadians((lat1 + lat2) / 2);
-    double dist_norte_sur = radio * dlat_rad;
-    double dist_este_oeste = radio * dlon_rad * Math.cos(lat_media_rad);
-
-    // Calculamos la distancia total entre dos puntos
-    double distancia_total = dist_norte_sur + dist_este_oeste;
-
-    // Evidentemente devolvemos la distancia total entre los dos puntos
-    return distancia_total;
 }
